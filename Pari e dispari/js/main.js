635ee3c749710dd8d79c6c51eb95e60eddfaf59a
bonvi83 <bonvi83@gmail.com>
2024-01-17 20:03:40 +0100

    add

--- Pari e dispari/js/main.js
+++ Pari e dispari/js/main.js
@@ -5,19 +5,53 @@ let numeroUtente = parseInt (prompt("Scegli un numero da 1 a 5."));
 console.log(`numero scelto ${numeroUtente}`);
 
 
-// genero (spero) numeri random
-function numeroRandom(min, max){
-
-    const newRandom = Math.floor(Math.random() * (max - min) + min);
-    return newRandom
+// controllo se è numero o meno   MA LO DEVE SCEGLIERE DI SOLO 2 CIFRE E DEVO ANCHE CONTROLLARE SE SCRIVE PARI O DISPARI
+if (isNaN(numeroUtente)){
+    alert (`Devi inserire solo numeri, riprova.`)
 }
 
-let numeroCasuale = numeroRandom (1, 5);
-console.log(`numero scelto dal computer:${numeroCasuale}`); 
+
+// genero (spero) numeri random
+function numeroRandom(min, max){
+    const newRandom = Math.floor(Math.random() * (max - min) + min);
+    return newRandom;
+}
+
+
+// stampo il numero del pc (spero)
+let numeroPc = numeroRandom (1, 5);
+console.log(`numero scelto dal pc:${numeroPc}`); 
+
+
+// sommo i numeri...
+let risultato = numeroUtente + numeroPc;
+console.log(`la somma è: ` + risultato); 
+
+
+// funzione per sapere se il numero è pari o dispari
+function scelta(numero){
+    if (numero % 2 == 0) {
+        valore = `pari`
+        console.log(`il numero è pari`); 
+    } else {
+        valore = `dispari`
+        console.log(`il numero è dispari`); 
+    }
+    return numero % 2 == 0;
+}
+console.log(scelta(risultato)); 
+
+
+// stampo il vincitore
+if (pariDispari == risultato){
+    alert(`HAI VINTO`);
+} else {
+    alert(`HAI PERSO`);
+}
 
 
 
-// OVVIAMENTE NON FUNZIONA NULLA :-(
+// OVVIAMENTE NON SI COMPORTA COME VORREI :-(
 
 
 
@@ -27,21 +61,19 @@ console.log(`numero scelto dal computer:${numeroCasuale}`);
 
 
 
-
-
-
+// VELOCE TUTORAGGIO CON MATTIA
 
 
 // function somma(a, b){
 //     let risultato = a + b;
 //     return risultato;
-
 // }
 
 
 // function ciao(nome){
 //     console.log("ciao " + nome)
 // }
+
 
 // let abbello;
 // let result = somma(3 , 2);
