3fb1598697c81949628e5955bf25aa3e4abbb8a3
bonvi83 <bonvi83@gmail.com>
2024-01-17 15:17:00 +0100

    add palidroma

---
+++ Palidroma/js/main.js
@@ -0,0 +1,6 @@
+// dichiaro le due variabili
+let utente;
+// let risultato = document.getElementById(risultato);
+
+// chiedo all'utente di digitare una parola
+utente = prompt("Inserisci una parola e ti dirò se è palindroma");
