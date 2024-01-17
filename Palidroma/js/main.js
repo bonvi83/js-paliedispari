088a6b26782c04fef90743486d90e54e65ab1799
bonvi83 <bonvi83@gmail.com>
2024-01-17 15:38:24 +0100

    palindroma ovviamente non funziona

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,6 +1,17 @@
-// dichiaro le due variabili
 let utente;
-// let risultato = document.getElementById(risultato);
+let risultato = document.getElementById("risposta");
+
 
 // chiedo all'utente di digitare una parola
 utente = prompt("Inserisci una parola e ti dirò se è palindroma");
+
+
+function palindroma(parola) {
+    let lunghezza = parola.length;
+    for (var i = 0; i <= lunghezza / 2;)
+        if (parola[i++] == parola [lunghezza - i]) return "NON è palindroma";
+    return "è palindroma"
+}
+
+
+risposta.innerHTML = (palindroma(utente))
