ce9a4f0300219059ccd4b8378cb912942ad92c57
bonvi83 <bonvi83@gmail.com>
2024-01-17 17:18:03 +0100

    add pari e dispari

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,17 +1,19 @@
 let utente;
-let risultato = document.getElementById("risposta");
 
 
 // chiedo all'utente di digitare una parola
 utente = prompt("Inserisci una parola e ti dirò se è palindroma");
 
 
+// costruzione formula
 function palindroma(parola) {
     let lunghezza = parola.length;
     for (var i = 0; i <= lunghezza / 2;)
-        if (parola[i++] == parola [lunghezza - i]) return "NON è palindroma";
-    return "è palindroma"
+    if (parola[i++] !== parola [lunghezza - i]) return "NON palindroma";
+    return "palindroma"
 }
 
 
-risposta.innerHTML = (palindroma(utente))
+// comunico il risultato
+let risultato = document.getElementById("risposta");
+risultato.innerHTML = (palindroma(utente))
---
+++ Pari e dispari/js/main.js
@@ -0,0 +1,30 @@
+let utente;
+let pc;
+
+
+// funzione per creare numeri random
+function numberRandom (){
+    return Math.floor(Math.random() * 5 + 1);
+}
+
+
+
+// comunico il risultato
+// let risultato = document.getElementById("risposta");
+// risultato.innerHTML = (palindroma(utente))
+
+function somma(a, b){
+    let risultato = a + b;
+    return risultato;
+
+}
+
+
+function ciao(nome){
+    console.log("ciao " + nome)
+}
+
+let abbello;
+let result = somma(3 , 2);
+let result2 = somma(1 , 2);
+ciao("Anna");
